58804629f495ac243354bcfc6a9a47f8d83bc1ef
Daniil Moiseev <33635935+daniilmoiseev@users.noreply.github.com>
2021-04-27 23:57:40 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,7 +5,67 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+  private:
+  T *arr;
+  int size;
+  int begin, end;
+  int count;
+public:
+  TPQueue() :
+    size(100),
+    begin(0), end(0), count(0) {
+    arr = new T[size + 1];
+  }
+  ~TPQueue() {
+    delete[] arr;
+  }
+
+  void push(const T &item) {
+    assert(count < size);
+
+    T *mas = new T[count];
+    bool tr = false;
+    if (end != 0) {
+      for (int i = 0; i < count; i++) {
+        if (item.prior > arr[end - 1].prior) {
+          mas[i] = arr[--end];
+          tr = true;
+        }
+      }
+      arr[end++] = item;
+    } else
+      arr[end++] = item;
+
+    if (tr) {
+      for (int i = count - 1; i >= 0; i--)
+        arr[end++] = mas[i];
+    }
+    count++;
+
+    if (end > size)
+      end -= size + 1;
+  }
+  T pop() {
+    assert(count > 0);
+
+    T item = arr[begin++];
+    count--;
+
+    if (begin > size)
+      begin -= size + 1;
+
+    return item;
+  }
+  T get() const {
+    assert(count > 0);
+    return arr[begin];
+  }
+  bool isEmpty() const {
+    return count == 0;
+  }
+  bool isFull() const {
+    return count == size;
+  }
 };
 
 struct SYM {
